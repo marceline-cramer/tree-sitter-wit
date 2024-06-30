40107c8aab09f1adfe3e808176a55d69b0b27905
marceline-cramer <cramermarceline@gmail.com>
2024-06-30 16:09:41 -0600

    fix indents queries

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,13 +1,13 @@
 [
-  (use_items)
-  (fields)
-  (variant_items)
-  (variant_payload)
-  (flags)
-  (enum_items)
-  (union_items)
-  (args)
-  (resource_items)
+  (world_items)
+  (interface_items)
+  (include_names_list)
+  (named_type_list)
+  (use_names_list)
+  (record_fields)
+  (flags_fields)
+  (variant_cases)
+  (enum_cases)
 ] @indent
 
 [ "}" ")" ] @outdent
